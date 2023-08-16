e1c0e9706a8befb9fff2a85bcd3cd4b9da6525e7
RevanthKumar <revanani1801@gmail.com>
2023-08-16 23:42:19 +0530

    iconshost

--- icons.js
+++ icons.js
@@ -2,439 +2,439 @@ const icons = {
   200: {
     label: "thunderstorm with light rain",
     icon: "storm-showers",
-    img:"../icons/thunderstorm-hail.svg"
+    img:"./thunderstorm-hail.svg"
   },
 
   201: {
     label: "thunderstorm with rain",
     icon: "storm-showers",
-    img:"../icons/thunderstorm-hail.svg"
+    img:"./thunderstorm-hail.svg"
   },
 
   202: {
     label: "thunderstorm with heavy rain",
     icon: "storm-showers",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   210: {
     label: "light thunderstorm",
     icon: "storm-showers",
-    img:"../icons/thunderstorm-hail.svg"
+    img:"./thunderstorm-hail.svg"
   },
 
   211: {
     label: "thunderstorm",
     icon: "thunderstorm",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   212: {
     label: "heavy thunderstorm",
     icon: "thunderstorm",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   221: {
     label: "ragged thunderstorm",
     icon: "thunderstorm",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   230: {
     label: "thunderstorm with light drizzle",
     icon: "storm-showers",
-    img:"../icons/thunderstorm-hail.svg"
+    img:"./thunderstorm-hail.svg"
   },
 
   231: {
     label: "thunderstorm with drizzle",
     icon: "storm-showers",
-    img:"../icons/thunderstorm-hail.svg"
+    img:"./thunderstorm-hail.svg"
   },
 
   232: {
     label: "thunderstorm with heavy drizzle",
     icon: "storm-showers",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   300: {
     label: "light intensity drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   301: {
     label: "drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   302: {
     label: "heavy intensity drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   310: {
     label: "light intensity drizzle rain",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   311: {
     label: "drizzle rain",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   312: {
     label: "heavy intensity drizzle rain",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   313: {
     label: "shower rain and drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   314: {
     label: "heavy shower rain and drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   321: {
     label: "shower drizzle",
     icon: "sprinkle",
-    img:"../icons/drizzle.svg"
+    img:"./drizzle.svg"
   },
 
   500: {
     label: "light rain",
     icon: "rain",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   501: {
     label: "moderate rain",
     icon: "rain",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   502: {
     label: "heavy intensity rain",
     icon: "rain",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   503: {
     label: "very heavy rain",
     icon: "rain",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   504: {
     label: "extreme rain",
     icon: "rain",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   511: {
     label: "freezing rain",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   520: {
     label: "light intensity shower rain",
     icon: "showers",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   521: {
     label: "shower rain",
     icon: "showers",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   522: {
     label: "heavy intensity shower rain",
     icon: "showers",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   531: {
     label: "ragged shower rain",
     icon: "showers",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   600: {
     label: "light snow",
     icon: "snow",
-    img:"../icons/snow.svg"
+    img:"./snow.svg"
   },
 
   601: {
     label: "snow",
     icon: "snow",
-    img:"../icons/snow.svg"
+    img:"./snow.svg"
   },
 
   602: {
     label: "heavy snow",
     icon: "snow",
-    img:"../icons/snow.svg"
+    img:"./snow.svg"
   },
 
   611: {
     label: "sleet",
     icon: "sleet",
-    img:"../icons/sleet.svg"
+    img:"./sleet.svg"
   },
 
   612: {
     label: "shower sleet",
     icon: "sleet",
-    img:"../icons/sleet.svg"
+    img:"./sleet.svg"
   },
 
   615: {
     label: "light rain and snow",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   616: {
     label: "rain and snow",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   620: {
     label: "light shower snow",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   621: {
     label: "shower snow",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   622: {
     label: "heavy shower snow",
     icon: "rain-mix",
-    img:"../icons/rain.svg"
+    img:"./rain.svg"
   },
 
   701: {
     label: "mist",
     icon: "sprinkle",
-    img:"../icons/fog.svg"
+    img:"./fog.svg"
   },
 
   711: {
     label: "smoke",
     icon: "smoke",
-    img:"../icons/fog.svg"
+    img:"./fog.svg"
   },
 
   721: {
     label: "haze",
     icon: "day-haze",
-    img:"../icons/hail.svg"
+    img:"./hail.svg"
   },
 
   731: {
     label: "sand, dust whirls",
     icon: "cloudy-gusts",
-    img : "../icons/wind.svg"
+    img : "./wind.svg"
   },
 
   741: {
     label: "fog",
     icon: "fog",
-    img:"../icons/fog.svg"
+    img:"./fog.svg"
   },
 
   751: {
     label: "sand",
     icon: "cloudy-gusts",
-    img : "../icons/wind.svg"
+    img : "./wind.svg"
   },
 
   761: {
     label: "dust",
     icon: "dust",
-    img : "../icons/wind.svg"
+    img : "./wind.svg"
   },
 
   762: {
     label: "volcanic ash",
     icon: "smog",
-    img:"../icons/fog.svg"
+    img:"./fog.svg"
   },
 
   771: {
     label: "squalls",
     icon: "day-windy",
-    img:"../icons/wind.svg"
+    img:"./wind.svg"
   },
 
   781: {
     label: "tornado",
     icon: "tornado",
-    img:"../icons/wind.svg"
+    img:"./wind.svg"
   },
 
   800: {
     label: "clear sky",
     icon: "sunny",
-    img:"../icons/clear-day.svg"
+    img:"./clear-day.svg"
   },
 
   801: {
     label: "few clouds",
     icon: "cloudy",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   802: {
     label: "scattered clouds",
     icon: "cloudy",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   803: {
     label: "broken clouds",
     icon: "cloudy",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   804: {
     label: "overcast clouds",
     icon: "cloudy",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   900: {
     label: "tornado",
     icon: "tornado",
-    img:"../icons/wind.svg"
+    img:"./wind.svg"
   },
 
   901: {
     label: "tropical storm",
     icon: "hurricane",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   902: {
     label: "hurricane",
     icon: "hurricane",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   903: {
     label: "cold",
     icon: "snowflake-cold",
-    img:"../icons/snowflake2.svg"
+    img:"./snowflake2.svg"
   },
 
   904: {
     label: "hot",
     icon: "hot",
-    img:"../icons/clear-day.svg"
+    img:"./clear-day.svg"
   },
 
   905: {
     label: "windy",
     icon: "windy",
-    img:"../icons/wind.svg"
+    img:"./wind.svg"
   },
 
   906: {
     label: "hail",
     icon: "hail",
-    img:"../icons/hail.svg"
+    img:"./hail.svg"
   },
 
   951: {
     label: "calm",
     icon: "sunny",
-    img:"../icons/clear-day.svg"
+    img:"./clear-day.svg"
   },
 
   952: {
     label: "light breeze",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   953: {
     label: "gentle breeze",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   954: {
     label: "moderate breeze",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   955: {
     label: "fresh breeze",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   956: {
     label: "strong breeze",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   957: {
     label: "high wind, near gale",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   958: {
     label: "gale",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   959: {
     label: "severe gale",
     icon: "cloudy-gusts",
-    img:"../icons/cloudy.svg"
+    img:"./cloudy.svg"
   },
 
   960: {
     label: "storm",
     icon: "thunderstorm",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   961: {
     label: "violent storm",
     icon: "thunderstorm",
-    img:"../icons/thunderstorm.svg"
+    img:"./thunderstorm.svg"
   },
 
   962: {
     label: "hurricane",
     icon: "cloudy-gusts",
-    img : "../icons/cloudy.svg"
+    img : "./cloudy.svg"
   },
 };
 
